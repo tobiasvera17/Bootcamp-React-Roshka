363696a302513b5dc4c70c124519c4225e033cb9
Tobias Vera <tobiasvera2011@gmail.com>
2023-05-16 15:48:47 -0400

    restablecer estilo del boton y del input

--- semana 2/Practica 11 - Formulario Bootstrap/code.js
+++ semana 2/Practica 11 - Formulario Bootstrap/code.js
@@ -2,9 +2,20 @@ const tipoDeDocumento = document.getElementById("inputTipoDeDocumento")
 const numeroDeDocumento = document.getElementById("inputNumeroDeDocumento")
 const buttonEnviar = document.getElementById("buttonEnviar")
 
+
+tipoDeDocumento.addEventListener("change", () => {
+    if (tipoDeDocumento.value !== "1") {
+        // Restablecer estilo y disponibilidad del botón de envío
+        numeroDeDocumento.removeAttribute("style");
+        buttonEnviar.removeAttribute("disabled");
+    }
+});
+
+
 numeroDeDocumento.addEventListener("input", () => {
     if(tipoDeDocumento.value == "1"){
         let input = numeroDeDocumento.value;
+        console.log(input)
         const pattern = /^\d+-\d{1}$/
         if(pattern.test(input)){
             buttonEnviar.removeAttribute("disabled")
