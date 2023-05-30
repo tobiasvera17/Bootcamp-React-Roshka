9eebeade65245c3410d01be046c61b577433bab2
Tobias Vera <tobiasvera2011@gmail.com>
2023-05-30 16:54:11 -0400

    formateo de codigo

--- semana 3/Practica 19 - Donacion de Sangre con Mapa React/donacion-sangre/src/GenerarMapaLocales.jsx
+++ semana 3/Practica 19 - Donacion de Sangre con Mapa React/donacion-sangre/src/GenerarMapaLocales.jsx
@@ -15,67 +15,66 @@ import { useState } from "react";
 
 let locales = [
   {
-  "id": 1,
-  "local_donacion": "EL CANTARO",
-  "longitud": -56.820182,
-  "latitud": -27.387026,
-  "creado_por": null,
-  "direccion": "Av. Paraguay Casi Mcal. Lopez 343",
-  "hora_apertura": 8,
-  "hora_cierre": 21
+    id: 1,
+    local_donacion: "EL CANTARO",
+    longitud: -56.820182,
+    latitud: -27.387026,
+    creado_por: null,
+    direccion: "Av. Paraguay Casi Mcal. Lopez 343",
+    hora_apertura: 8,
+    hora_cierre: 21,
   },
   {
-  "id": 2,
-  "local_donacion": "FCA - CAMPUS UNP",
-  "longitud": -58.288459,
-  "latitud": -26.879844,
-  "creado_por": null,
-  "direccion": "Eusebio Ayala casi Intendentes Militares 123",
-  "hora_apertura": 8,
-  "hora_cierre": 21
+    id: 2,
+    local_donacion: "FCA - CAMPUS UNP",
+    longitud: -58.288459,
+    latitud: -26.879844,
+    creado_por: null,
+    direccion: "Eusebio Ayala casi Intendentes Militares 123",
+    hora_apertura: 8,
+    hora_cierre: 21,
   },
   {
-  "id": 3,
-  "local_donacion": "EL CANTARO",
-  "longitud": -56.820182,
-  "latitud": -27.387026,
-  "creado_por": null,
-  "direccion": "Av. Paraguay Casi Mcal. Lopez 343",
-  "hora_apertura": 8,
-  "hora_cierre": 21
+    id: 3,
+    local_donacion: "EL CANTARO",
+    longitud: -56.820182,
+    latitud: -27.387026,
+    creado_por: null,
+    direccion: "Av. Paraguay Casi Mcal. Lopez 343",
+    hora_apertura: 8,
+    hora_cierre: 21,
   },
   {
-  "id": 4,
-  "local_donacion": "Surubi-i, Mariano",
-  "longitud": -57.521369,
-  "latitud": -25.194156,
-  "creado_por": null,
-  "direccion": "Ruta Transchaco 1232 casi las Lomas Km. 3",
-  "hora_apertura": null,
-  "hora_cierre": null
+    id: 4,
+    local_donacion: "Surubi-i, Mariano",
+    longitud: -57.521369,
+    latitud: -25.194156,
+    creado_por: null,
+    direccion: "Ruta Transchaco 1232 casi las Lomas Km. 3",
+    hora_apertura: null,
+    hora_cierre: null,
   },
   {
-  "id": 5,
-  "local_donacion": "SODEP",
-  "longitud": -57.583222,
-  "latitud": -25.308953,
-  "creado_por": null,
-  "direccion": "Av. Paraguay Casi Mcal. Lopez 343",
-  "hora_apertura": 8,
-  "hora_cierre": 21
+    id: 5,
+    local_donacion: "SODEP",
+    longitud: -57.583222,
+    latitud: -25.308953,
+    creado_por: null,
+    direccion: "Av. Paraguay Casi Mcal. Lopez 343",
+    hora_apertura: 8,
+    hora_cierre: 21,
   },
   {
-  "id": 6,
-  "local_donacion": "ROSHKA",
-  "longitud": -57.6100684,
-  "latitud": -25.2818691,
-  "creado_por": null,
-  "direccion": null,
-  "hora_apertura": null,
-  "hora_cierre": null
-  }
-  ]
-  
+    id: 6,
+    local_donacion: "ROSHKA",
+    longitud: -57.6100684,
+    latitud: -25.2818691,
+    creado_por: null,
+    direccion: null,
+    hora_apertura: null,
+    hora_cierre: null,
+  },
+];
 
 const GenerarMapaLocales = () => {
   const [showLocales, setShowLocales] = useState(false);
@@ -96,10 +95,17 @@ const GenerarMapaLocales = () => {
               <Marker key={index} position={[item.latitud, item.longitud]}>
                 <Popup>
                   Nombre del local: {item.local_donacion} <br />
-                  Dirección: { item.direccion != null ? item.direccion : "N/D"} <br />
-                  Horario de Apertura: {item.hora_apertura != null ? item.hora_apertura + "hs." : "N/D"}
+                  Dirección: {item.direccion != null
+                    ? item.direccion
+                    : "N/D"}{" "}
                   <br />
-                  Horario de Cierre: {item.hora_cierre != null ? item.hora_cierre + "hs." : "N/D"}
+                  Horario de Apertura:{" "}
+                  {item.hora_apertura != null
+                    ? item.hora_apertura + "hs."
+                    : "N/D"}
+                  <br />
+                  Horario de Cierre:{" "}
+                  {item.hora_cierre != null ? item.hora_cierre + "hs." : "N/D"}
                   <br />
                   <a
                     href={
