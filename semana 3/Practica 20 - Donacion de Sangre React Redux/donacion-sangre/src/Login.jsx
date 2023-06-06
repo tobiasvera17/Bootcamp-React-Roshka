50a292a904cb2d5bc450ad72c865719a0799216e
Tobias Vera <tobiasvera2011@gmail.com>
2023-06-06 10:38:21 -0400

    formateo de codigo

--- semana 3/Practica 20 - Donacion de Sangre React Redux/donacion-sangre/src/Login.jsx
+++ semana 3/Practica 20 - Donacion de Sangre React Redux/donacion-sangre/src/Login.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 
 const Login = () => {
   const navigate = useNavigate();
