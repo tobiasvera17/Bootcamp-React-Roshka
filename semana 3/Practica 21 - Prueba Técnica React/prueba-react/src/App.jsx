5e288e21934bbb04c87e2676446d259e3d40d2a8
Tobias Vera <tobiasvera2011@gmail.com>
2023-06-07 21:48:26 -0400

    agregar delete characters, deja de funcionar pagination

--- semana 3/Practica 21 - Prueba Técnica React/prueba-react/src/App.jsx
+++ semana 3/Practica 21 - Prueba Técnica React/prueba-react/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import Cards from "./components/Cards/Cards";
 import Pagination from "./components/Pagination/Pagination";
+import CardsDos from "./components/Cards/CardsDos";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
@@ -9,7 +10,6 @@ function App() {
   const [pageNumber, setPageNumber] = useState(1);
   const [charactersData, setCharactersData] = useState([]);
   let { info, results } = charactersData;
-
   let request = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
 
   useEffect(() => {
@@ -19,25 +19,29 @@ function App() {
     })();
   }, [request]);
 
-  console.log(charactersData);
   return (
     <>
-    <div className="container text-center">
-      <h1>Prueba Técnica React</h1>
-    </div>
-      <div className="container mt-5 mb-5">
-        <div className="row">
-          <div className="col-1"></div>
-          <div className="col-10">
-            <div className="row">
-              <Cards results={results} />
-            </div>
-          </div>
-          <div className="col-1"></div>
+        <div className="container text-center">
+          <h1>Prueba Técnica React</h1>
         </div>
-      </div>
-
-      <Pagination info={info} pageNumber={pageNumber} setPageNumber={setPageNumber} />
+        <div className="container mt-5 mb-5">
+          <div className="row">
+            <div className="col-1"></div>
+            <div className="col-10">
+              <div className="row">
+              {results && (
+                <Cards results={results} />
+               )}
+              </div>
+            </div>
+            <div className="col-1"></div>
+          </div>
+        </div>
+          <Pagination
+            info={info}
+            pageNumber={pageNumber}
+            setPageNumber={setPageNumber}
+          />
     </>
   );
 }
--- semana 3/Practica 21 - Prueba Técnica React/prueba-react/src/components/Cards/Cards.jsx
+++ semana 3/Practica 21 - Prueba Técnica React/prueba-react/src/components/Cards/Cards.jsx
@@ -1,10 +1,24 @@
+import { useEffect, useState } from "react";
 
 const Cards = ({ results }) => {
+  const [charactersList, setCharactersList] = useState(results);
+
+  // useEffect(() => {
+  //     setCharactersList(results);
+  //     console.log(charactersList)
+  //   }, []);
+
+  const deleteCard = (id) => {
+    const result = charactersList.filter(character => character.id != id);
+    setCharactersList(result);
+}
+
   return (
     <>
-      {results &&
-        results.map((item) => {
-          let { id, image, name, species, gender, origin, location, status} = item;
+      {charactersList && (
+        charactersList.map((item) => {
+          let { id, image, name, species, gender, origin, location, status } =
+            item;
           return (
             <div key={id} className="col-4 mb-5">
               <div className=" card p-2">
@@ -21,13 +35,40 @@ const Cards = ({ results }) => {
                     <div className="fs-6">Last location</div>
                     <div className="fs-5">{location.name}</div>
                     <div className="fs-6">Status</div>
-                    <div> <span className={`badge + ${status == "Alive" ? "bg-success" : status == "Dead" ? "bg-danger" : status == "unknown" ? "bg-secondary" : ""}`}>{status}</span></div>
+                    <div>
+                      {" "}
+                      <span
+                        className={`badge + ${
+                          status == "Alive"
+                            ? "bg-success"
+                            : status == "Dead"
+                            ? "bg-danger"
+                            : status == "unknown"
+                            ? "bg-secondary"
+                            : ""
+                        }`}
+                      >
+                        {status}
+                      </span>
+                    </div>
+                    <div className="d-flex justify-content-around my-4">
+                      <button className="btn btn-primary">Editar</button>
+                      <button
+                        className="btn btn-danger"
+                        onClick={() => {
+                          deleteCard(id);
+                        }}
+                      >
+                        Eliminar
+                      </button>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           );
-        })}
+        })
+        )}
     </>
   );
 };
