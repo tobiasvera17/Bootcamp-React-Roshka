382b44997a4440681bc52621da170a3659c55d0d
Tobias Vera <tobiasvera2011@gmail.com>
2023-06-09 17:11:36 -0400

    publicar initial commit

--- semana 3/Practica 22 - Busca Mascotas React/busca-mascotas/src/components/Publicar.tsx
+++ semana 3/Practica 22 - Busca Mascotas React/busca-mascotas/src/components/Publicar.tsx
@@ -1,9 +1,52 @@
-import React from 'react'
-
 const Publicar = () => {
   return (
-    <div>Publicar</div>
-  )
-}
+    <div className="container-fluid p-5 m-0 index-component-container">
+      <div className="row align-items-center justify-content-center">
+        <div className="col-8">
+        <form>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+          <div id="emailHelp" className="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+          />
+        </div>
+        <div className="mb-3 form-check">
+          <input
+            type="checkbox"
+            className="form-check-input"
+            id="exampleCheck1"
+          />
+          <label className="form-check-label" htmlFor="exampleCheck1">
+            Check me out
+          </label>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+        </div>
+      </div>
+      
+    </div>
+  );
+};
 
-export default Publicar
+export default Publicar;
